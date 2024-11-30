c2e5f4c55563f36f26403b538f8f27f76cdab274
Juan Quirino <juanquirinoc@hotmail.com>
2024-11-29 20:39:28 -0800

    updates

---
+++ PixelBoardConstants.cs
@@ -0,0 +1,7 @@
+public class PixelBoardConstants
+{
+    public const string BoardCacheKey = "MainBoard";
+    public const int Rows = 100;
+    public const int Cols = 100;
+    public const string DefaultColor = "#FFFFFF";
+}
